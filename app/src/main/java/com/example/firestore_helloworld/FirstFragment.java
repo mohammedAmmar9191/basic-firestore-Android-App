fd91c7173b26949ab993191762c7b698b087c198
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-05-14 20:47:28 +0200

    add project source files

---
+++ app/src/main/java/com/example/firestore_helloworld/FirstFragment.java
@@ -0,0 +1,68 @@
+package com.example.firestore_helloworld;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.firestore_helloworld.databinding.FragmentFirstBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    private FirebaseDatabase rootNode;
+    private DatabaseReference reference;
+    //FirebaseFirestore db = FirebaseFirestore.getInstance();
+    //CollectionReference cities = db.collection("cities");
+
+    Map<String, Object> user = new HashMap<>();
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("company1");
+                reference.setValue("a7a");
+                System.out.println(reference);
+                Toast.makeText(view.getContext(),"success",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
